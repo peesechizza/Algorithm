54c04172d025c0b8cefbac637acab400e475d2b0
a1dotls <110324109+peesechizza@users.noreply.github.com>
2023-11-17 16:35:28 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		int A = scanner.nextInt();
-		int B = scanner.nextInt();
-		
-			System.out.println(A + B);
+        String[] str = br.readLine().split(" ");
 
-	}
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
 
+        System.out.println(a + b);
+    }
 }
