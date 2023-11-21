73ce294d8a4d8e1d3eb14113d51ceedcc995d4fd
a1dotls <110324109+peesechizza@users.noreply.github.com>
2023-11-21 11:48:45 +0900

    Update 문자열 출력하기 BufferedReader 사용.java

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,10 +1,23 @@
+// import java.util.Scanner;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String a = sc.next();
+        
+//         System.out.println(a);
+//     }
+// }
+
 import java.util.Scanner;
+import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
         
-        System.out.println(a);
+        System.out.println(str);
+            
     }
 }
