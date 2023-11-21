9fb777848324d7a49df77894d14b598a5a5712a6
a1dotls <110324109+peesechizza@users.noreply.github.com>
2023-11-21 11:59:18 +0900

    Update a와 b 출력하기.java BufferedReader 사용

--- 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.java
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -1,13 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+        
+        int a = Integer.parseInt(str.nextToken());
+        int b = Integer.parseInt(str.nextToken());
+        
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         
+            
+        
     }
 }
+
+// import java.util.Scanner;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+
+//         System.out.println("a = " + a);
+//         System.out.println("b = " + b);
+        
+//     }
+// }
