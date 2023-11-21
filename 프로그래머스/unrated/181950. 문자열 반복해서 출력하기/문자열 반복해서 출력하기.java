ab5f998e48cd18830673283b6f4e2ddebce6469b
a1dotls <110324109+peesechizza@users.noreply.github.com>
2023-11-21 12:33:17 +0900

    Update 문자열 반복해서 출력하기.java BufferedReader 사용

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -1,3 +1,4 @@
+// BufferedReader 사용
 import java.util.*;
 import java.io.*;
 
@@ -11,6 +12,23 @@ public class Solution {
         
         System.out.println(str.repeat(n));
         
-        
     }
 }
+
+// Scanner 사용
+// import java.util.*;
+// import java.io.*;
+
+// public class Solution {
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+//         String str = st.nextToken();
+//         int n = Integer.parseInt(st.nextToken());
+        
+//         System.out.println(str.repeat(n));
+        
+        
+//     }
+// }
