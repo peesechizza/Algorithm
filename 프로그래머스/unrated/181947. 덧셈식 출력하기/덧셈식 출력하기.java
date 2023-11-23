0b0d858fb2f5d29cdc9a4d6d61fddb740433df59
a1dotls <110324109+peesechizza@users.noreply.github.com>
2023-11-23 18:21:40 +0900

    Update 덧셈식 출력하기.java BufferedReader 사용

--- 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.java
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -1,12 +1,33 @@
-import java.util.Scanner;
+// BufferedReader 사용
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
         int answer = a + b;
         
         System.out.printf("%d + %d = %d", a, b, answer);
+        
+
     }
 }
+
+
+// Scanner 사용
+// import java.util.Scanner;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int answer = a + b;
+        
+//         System.out.printf("%d + %d = %d", a, b, answer);
+//     }
+// }
