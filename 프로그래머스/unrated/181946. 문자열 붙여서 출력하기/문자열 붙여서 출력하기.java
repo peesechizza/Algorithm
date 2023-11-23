cc438d9fdca31ac7b428453bd67ce1b72d33cdc2
a1dotls <110324109+peesechizza@users.noreply.github.com>
2023-11-23 18:34:47 +0900

    Update 문자열 붙여서 출력하기.java BufferedReader 사용

--- 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -1,11 +1,29 @@
-import java.util.Scanner;
+// BufferedReader 사용
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        System.out.printf("%s%s", a, b);
+        String str1 = st.nextToken();
+        String str2 = st.nextToken();
+        
+        System.out.printf("%s%s", str1, str2);
+        
     }
 }
+
+// Scanner 사용
+// import java.util.Scanner;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String a = sc.next();
+//         String b = sc.next();
+        
+//         System.out.printf("%s%s", a, b);
+//     }
+// }
