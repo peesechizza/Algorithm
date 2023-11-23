c1156d69aaad8640896641df508c54826cf4becb
a1dotls <110324109+peesechizza@users.noreply.github.com>
2023-11-23 18:46:37 +0900

    Update 문자열 붙여서 출력하기.java replaceAll / concat 사용

--- 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -1,3 +1,4 @@
+// replaceAll 사용
 import java.util.*;
 import java.io.*;
 
@@ -5,12 +6,23 @@ public class Solution {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print(br.readLine().replaceAll(" ", ""));
-//         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-//         String str1 = st.nextToken();
-//         String str2 = st.nextToken();
-        
-//         System.out.print(str1.concat(str2));
-        
     }
 }
+
+
+// concat 사용
+// import java.util.*;
+// import java.io.*;
+
+// public class Solution {
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//          StringTokenizer st = new StringTokenizer(br.readLine());
+        
+//          String str1 = st.nextToken();
+//          String str2 = st.nextToken();
+        
+//          System.out.print(str1.concat(str2));
+        
+//     }
+// }
